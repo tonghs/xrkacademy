074b2591008e426285f07873583c9954c258fffb
tonghs <tonghuashuai@126.com>
2017-03-06 21:18:15 +0800

    fix

--- handler/root.py
+++ handler/root.py
@@ -16,3 +16,9 @@ class Index(BaseHandler):
 class Login(tornado.web.RequestHandler):
     def get(self):
         self.render('../template/login.html')
+
+
+@route('/introduction')
+class Introduction(BaseHandler):
+    def get(self):
+        self.render()
